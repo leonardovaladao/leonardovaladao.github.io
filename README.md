14f05bc58e90211f148646a316934315b8e749cc
leonardovaladao <leonardo.m.valadao@hotmail.com>
2021-12-09 16:36:34 -0300

    Publish old version into new branch

---
+++ README.md
@@ -0,0 +1,2 @@
+# leonardovaladao.github.io
+<p> Click <a href="https://leonardovaladao.github.io/index">here</a> to access my portfolio website.</p>
---
+++ precache-manifest.0c0416c4318ff8bf2f1f6c755fb75930.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ff60ef2013ecb7c169c7f88e8c9e54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47f8ebe1a16a9349ee27",
+    "url": "./static/css/main.35f8d1ce.chunk.css"
+  },
+  {
+    "revision": "a92c0618fdfcf2a2420b",
+    "url": "./static/js/2.c9e4864e.chunk.js"
+  },
+  {
+    "revision": "0d112491b31a7bd4d4bfb66601772864",
+    "url": "./static/js/2.c9e4864e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f8ebe1a16a9349ee27",
+    "url": "./static/js/main.c32725ff.chunk.js"
+  },
+  {
+    "revision": "3b0ba3529da5c1cd13d8",
+    "url": "./static/js/runtime-main.7dacd673.js"
+  },
+  {
+    "revision": "f6faa4cc49534f1cd2b6fefd66b68226",
+    "url": "./static/media/home-bg.f6faa4cc.jpg"
+  },
+  {
+    "revision": "5e0aa5529b40fa54b1826c8ee480effa",
+    "url": "./static/media/profile-img.5e0aa552.jpg"
+  }
+]);
